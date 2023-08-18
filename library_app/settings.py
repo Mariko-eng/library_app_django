aa1fe6fde9c581bd3074ad68de2e3b372d8c42e5
Mark Gabriel Yiga <yigamarkgabriel333@gmail.com>
2023-08-18 15:55:41 +0300

    Change to sqlite db

--- library_app/settings.py
+++ library_app/settings.py
@@ -88,19 +88,19 @@ WSGI_APPLICATION = 'library_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DATABASE_NAME"),
-        "USER": os.getenv("DATABASE_USER"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-        "HOST": os.getenv("DATABASE_HOST"),
-        "PORT": "5432",
-    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": os.getenv("DATABASE_NAME"),
+    #     "USER": os.getenv("DATABASE_USER"),
+    #     "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+    #     "HOST": os.getenv("DATABASE_HOST"),
+    #     "PORT": "5432",
+    # },
 
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
