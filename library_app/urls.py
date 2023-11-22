fe8abe796d23a7228dc2f69effbe42066f4a62cc
Mark Gabriel Yiga <yigamarkgabriel333@gmail.com>
2023-11-22 08:43:54 +0300

    media and static urls fix

--- library_app/urls.py
+++ library_app/urls.py
@@ -33,5 +33,6 @@ urlpatterns = [
     path('404/', TemplateView.as_view(template_name='404.html'), name='404'),
 ]
 
-urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
